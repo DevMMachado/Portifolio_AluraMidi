656e9540e9b0b36d14972181f4299878c489d770
DevMMachado <75995823+DevMMachado@users.noreply.github.com>
2023-03-17 11:09:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
-function tocaSom(idAudio) {
-    const elemento = document.querySelector(idAudio);
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-
-    if (elemento && elemento.localname === 'audio') {
+    if (elemento && elemento.localName === 'audio') {
         elemento.play();
     }
-    else if (elemento === null) {
-        alert('Elemento não encontrado ou seletor inválido.');
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
     }
 
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
-    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-        const tecla = listaDeTeclas[contador];
-        const instrumento = tecla.classList[1];
-        const idAudio = `#som_${instrumento}`;
-
-        tecla.onclick = function () {
-            tocaSom(idAudio);
-        }
-
-        tecla.onkeydown = function (evento) {
-            if (evento.code === 'Space' || evento.code === 'Enter') {
-                tecla.classList.add('ativa');
-            }
-        }
-        tecla.onkeyup = function () {
-            tecla.classList.remove('ativa');
-        }
-
-
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
+
+}
